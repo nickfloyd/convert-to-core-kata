7ee3f04d73fb7a103f0f1d5c4e1a9ee19d58b31c
Nick Floyd <nicholas.floyd.info@gmail.com>
2017-02-26 23:01:22 -0600

    init commit

---
+++ ConvertToCoreApp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConvertToCoreLib;
+
+namespace ConvertToCore
+{
+	public class ConvertToCoreApp
+	{
+		private void DoThing()
+		{
+			var helper = new Helpers();
+			var total = helper.Add(1, 2);
+
+		}
+	}
+}
---
+++ ConvertToCoreLib/Helpers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertToCoreLib
+{
+    public class Helpers
+    {
+	    public int Add(int first, int second)
+	    {
+		    return first + second;
+
+	    }
+    }
+}
